926953f51a9bae0cfd657aa2ea2a0a175c91dd05
oti <otiext@gmail.com>
2020-07-14 19:24:03 +0900

    prettier --write .

--- dist/index.js
+++ dist/index.js
@@ -3,72 +3,72 @@ module.exports.DecomojiColors = [
     name: "tomato",
     hex: "dd3b40",
     rgb: "221,59,64",
-    hsl: "358,86,59"
+    hsl: "358,86,59",
   },
   {
     name: "chocolate",
     hex: "c05b2c",
     rgb: "192,91,44",
-    hsl: "19,63,46"
+    hsl: "19,63,46",
   },
   {
     name: "gold",
     hex: "9f7e00",
     rgb: "159,126,0",
-    hsl: "48,100,31"
+    hsl: "48,100,31",
   },
   {
     name: "olive",
     hex: "688200",
     rgb: "104,130,0",
-    hsl: "72,100,25"
+    hsl: "72,100,25",
   },
   {
     name: "forest",
     hex: "008c22",
     rgb: "0,140,34",
-    hsl: "135,100,27"
+    hsl: "135,100,27",
   },
   {
     name: "teal",
     hex: "008780",
     rgb: "0,135,128",
-    hsl: "177,100,26"
+    hsl: "177,100,26",
   },
   {
     name: "cerulean",
     hex: "0081b1",
     rgb: "0,129,177",
-    hsl: "196,100,35"
+    hsl: "196,100,35",
   },
   {
     name: "denim",
     hex: "477f9b",
     rgb: "71,127,155",
-    hsl: "200,37,44"
+    hsl: "200,37,44",
   },
   {
     name: "danube",
     hex: "5d79aa",
     rgb: "93,121,170",
-    hsl: "218,31,52"
+    hsl: "218,31,52",
   },
   {
     name: "lavender",
     hex: "a156d2",
     rgb: "161,86,210",
-    hsl: "276,58,58"
+    hsl: "276,58,58",
   },
   {
     name: "azalea",
     hex: "d43892",
     rgb: "212,56,146",
-    hsl: "325,64,53"
+    hsl: "325,64,53",
   },
   {
     name: "coral",
     hex: "a36969",
     rgb: "163,105,105",
-    hsl: "0,24,53"
-  }
-]
+    hsl: "0,24,53",
+  },
+];
